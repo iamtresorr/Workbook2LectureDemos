500e3545737bbee847ace33d1ff1723c0d5d2ef5
Iradukunda Tresor <95381043+iamtresorr@users.noreply.github.com>
2023-04-24 15:44:15 -0400

    Create FirstCode.js

---
+++ LectureDemos/FirstCode.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var firstName;
+
+firstName = "Iradukunda";
+
+var greeting;
+greeting = "Hello! Your name is ";
+
+console.log("Your name is " + firstName);
+console.log(greeting + firstName);
+
+
+//Exercise 1 - variable naming practice
+
+/* var customerId;
+var customerFirstName;
+var customerMiddleName;
+var customerLastName;
+var customerGender;
+var customerDOB;
+var customerLicenseNumber;
+var customerPolicyNumber;
+*/
+
+//Exercise 2: words that CANNOT be usued as variable names
+/* 
+1. Break, switch, return, this, float, if, else, let, var, boolean
+2. Object, array, math, date, eval, undefined, name, string, function, valueOf
+3. Alert, prompt, focus, element, confirm, button, area, document, form, embedded.
+4. onload, onclick, onerror, onfocus, onmousedown, onsubmit
+*/
+
+//Exercise 3
+
+
+var fullName;
+var birthState;
+var birthCity;
+
+fullName = "Iradukunda Tresor";
+birthState = "Georgia";
+birthCity = "Atlanta";
